411ae4ac7b71cf6dfe0993a8aa5494a7aa2adf45
Robert Pliszka <robplisz@gmail.com>
2022-04-04 17:49:58 -0400

    Added dates to the current date plus 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 var locationFormEl = document.querySelector("#user-form");
 var locationInputEl = document.querySelector("#location");
+var currentDate = moment().format("M[/]D[/]YY");
+var tomorrowDate = moment().add(1, 'days').format("M[/]D[/]YY");
+var dayTwoDate = moment().add(2, 'days').format("M[/]D[/]YY");
+var dayThreeDate = moment().add(3, 'days').format("M[/]D[/]YY");
+var dayFourDate = moment().add(4, 'days').format("M[/]D[/]YY");
+var dayFiveDate = moment().add(5, 'days').format("M[/]D[/]YY");
 
 // From submit get input
 function formSubmitHaldler() {
@@ -67,10 +73,10 @@ function getLocationWeather(lat, lon) {
 };
 
 function displayWeatherResults(data) {
-    // City Name
+    // City Name and date
     var currentCityNameEl = document.querySelector(".jumbotron-header");
     var cityName = data.timezone.split("/")[1];
-    currentCityNameEl.textContent = cityName;
+    currentCityNameEl.textContent = cityName + " (" + currentDate + ")";
 
     // Current Temp
     var currentTempEl = document.querySelector("#current-temp");
@@ -84,6 +90,75 @@ function displayWeatherResults(data) {
     // Current Uv
     var currentUvEl = document.querySelector("#uv-style");
     currentUvEl.textContent = data.current.uvi;
+
+    // Day 1 Date
+    var firstDayDateEl = document.querySelector("#day-1-date");
+    firstDayDateEl.textContent = tomorrowDate;
+    // Day 1 temp
+    var firstDayTempEl = document.querySelector("#day-1-temp");
+    firstDayTempEl.textContent = data.daily[0].temp.day + "F";
+    // Day 1 wind
+    var firstDayWindEl = document.querySelector("#day-1-wind");
+    firstDayWindEl.textContent = data.daily[0].wind_speed + "MPH";
+    // Day 1 humidity
+    var firstDayHumidityEl = document.querySelector("#day-1-humidity");
+    firstDayHumidityEl.textContent = data.daily[0].humidity + "%";
+
+
+    // Day 2 Date
+    var secondDayDateEl = document.querySelector("#day-2-date");
+    secondDayDateEl.textContent = dayTwoDate;
+    // Day 2 temp
+    var secondDayTempEl = document.querySelector("#day-2-temp");
+    secondDayTempEl.textContent = data.daily[1].temp.day + "F";
+    // Day 2 wind
+    var secondDayWindEl = document.querySelector("#day-2-wind");
+    secondDayWindEl.textContent = data.daily[1].wind_speed + "MPH";
+    // Day 2 humidity
+    var secondDayHumidityEl = document.querySelector("#day-2-humidity");
+    secondDayHumidityEl.textContent = data.daily[1].humidity + "%";
+
+
+    // Day 3 Date
+    var thirdDayDateEl = document.querySelector("#day-3-date");
+    thirdDayDateEl.textContent = dayThreeDate;
+    // Day 3 temp
+    var thirdDayTempEl = document.querySelector("#day-3-temp");
+    thirdDayTempEl.textContent = data.daily[2].temp.day + "F";
+    // Day 3 wind
+    var thirdDayWindEl = document.querySelector("#day-3-wind");
+    thirdDayWindEl.textContent = data.daily[2].wind_speed + "MPH";
+    // Day 3 humidity
+    var thirdDayHumidityEl = document.querySelector("#day-3-humidity");
+    thirdDayHumidityEl.textContent = data.daily[2].humidity + "%";
+
+
+    // Day 4 Date
+    var fourthDayDateEl = document.querySelector("#day-4-date");
+    fourthDayDateEl.textContent = dayFourDate;
+    // Day 4 temp
+    var fourthDayTempEl = document.querySelector("#day-4-temp");
+    fourthDayTempEl.textContent = data.daily[3].temp.day + "F";
+    // Day 4 wind
+    var fourthDayWindEl = document.querySelector("#day-4-wind");
+    fourthDayWindEl.textContent = data.daily[3].wind_speed + "MPH";
+    // Day 4 humidity
+    var fourthDayHumidityEl = document.querySelector("#day-4-humidity");
+    fourthDayHumidityEl.textContent = data.daily[3].humidity + "%";
+
+
+    // Day 5 Date
+    var fifthDayDateEl = document.querySelector("#day-5-date");
+    fifthDayDateEl.textContent = dayFiveDate;
+    // Day 5 temp
+    var fifthDayTempEl = document.querySelector("#day-5-temp");
+    fifthDayTempEl.textContent = data.daily[4].temp.day + "F";
+    // Day 5 wind
+    var fifthDayWindEl = document.querySelector("#day-5-wind");
+    fifthDayWindEl.textContent = data.daily[4].wind_speed + "MPH";
+    // Day 5 humidity
+    var fifthDayHumidityEl = document.querySelector("#day-5-humidity");
+    fifthDayHumidityEl.textContent = data.daily[4].humidity + "%";
 
 }
 
